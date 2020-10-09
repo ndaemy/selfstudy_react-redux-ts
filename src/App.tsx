5b115fc25c6237d22f71f289eefe551e02c96d00
yuyaebean <yuyaebean@gmail.com>
2020-10-09 19:57:54 +0900

    Refactory code using typesafe-actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
+import Counter from './components/Counter';
 
 function App() {
-  return (
-    <>
-      <TodoInsert />
-      <TodoList />
-    </>
-  );
+  return <Counter />;
 }
 
 export default App;
